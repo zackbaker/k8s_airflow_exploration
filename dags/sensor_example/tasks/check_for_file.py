851f4e40b5d75c157318bfb3de3b57722be1d84d
Zack Baker <zack.baker@Mac0552Pro.local>
2020-03-27 11:28:36 -0600

    adding arguements

--- dags/sensor_example/tasks/check_for_file.py
+++ dags/sensor_example/tasks/check_for_file.py
@@ -1,6 +1,11 @@
 import logging
 
 
-def run():
+def run(ds, **kwargs):
+    logging.info('-------------------')
+    logging.info(ds)
+    logging.info('-------------------')
+    logging.info(kwargs)
+    logging.info('-------------------')
     logging.info('Hello World!')
     return True
