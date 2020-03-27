ef5e5adfc3e2e1c04d5360f36d8ce1be8064932a
Zack Baker <zack.baker@Mac0552Pro.local>
2020-03-27 10:53:07 -0600

    testing

--- custom_airflow_operators/file_creation.py
+++
@@ -1,6 +0,0 @@
-import Observer as Observer
-
-
-class Watcher:
-    def __init__(self, file_path):
-        self.observer = Observer()
--- dags/sensor_example/random_number_watcher.py
+++ dags/sensor_example/random_number_watcher.py
@@ -1,46 +1,54 @@
-# from airflow import DAG
-# from datetime import timedelta, datetime
-#
-# from airflow.contrib.kubernetes.volume import Volume
-# from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-#
-#
-# default_args = {
-#     'owner': 'airflow',
-#     'depends_on_past': False,
-#     'start_date': '2020-03-20 09:00:00',
-#     'email': ['airflow@example.com'],
-#     'email_on_failure': False,
-#     'email_on_retry': False,
-#     'retries': 0,
-#     'retry_delay': timedelta(minutes=5),
-# }
-#
-# dag = DAG(
-#     'cron_example',
-#     default_args=default_args,
-#     schedule_interval='*/5 * * * *',
-#     catchup=False
-# )
-#
-# volume_config = {
-#     'persistentVolumeClaim': {
-#         'claimName': 'file-store'
-#     }
-# }
-# volume = Volume(name='file-store', configs=volume_config)
-#
-# timestamp = datetime.now()
-#
-# create_file = KubernetesPodOperator(
-#     namespace='airflow',
-#     image="debian:9.4",
-#     cmds=["bash", "-c"],
-#     arguments=['echo $(($RANDOM%10000+10000)) >> /mnt/file-store/random_number-' + str(timestamp) + '.txt'],
-#     name="generate-random-number",
-#     task_id="generate-random-number",
-#     volumes=[volume],
-#     in_cluster=True,
-#     get_logs=True,
-#     dag=dag
-# )
+from airflow import DAG
+from datetime import timedelta, datetime
+
+from airflow.contrib.kubernetes.volume import Volume
+from airflow.contrib.kubernetes.volume_mount import VolumeMount
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': '2020-03-20 09:00:00',
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'cron_example',
+    default_args=default_args,
+    schedule_interval='*/5 * * * *',
+    catchup=False
+)
+
+volume_config = {
+    'persistentVolumeClaim': {
+        'claimName': 'file-store'
+    }
+}
+volume = Volume(name='file-store', configs=volume_config)
+volume_mount = VolumeMount(
+    'file-store',
+    mount_path='/mnt/file-store',
+    sub_path=None,
+    read_only=False
+)
+
+timestamp = datetime.now()
+
+create_file = KubernetesPodOperator(
+    namespace='airflow',
+    image="zackbaker/k8s_airflow_test:latest",
+    cmds=["python", "sensor_example/tasks/check_for_file.py"],
+    name="generate-random-number",
+    task_id="generate-random-number",
+    volumes=[volume],
+    volume_mounts=[volume_mount],
+    in_cluster=True,
+    get_logs=True,
+    dag=dag
+)
+
